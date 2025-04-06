ad6dfc9dc6a11f4f3a0fa675228024eb15792b47
wevedo <mribraahtech@gmail.com>
2025-04-06 08:08:32 +0300

    .

--- Ibrahim/adams.js
+++ Ibrahim/adams.js
@@ -21,7 +21,7 @@ const ev = {
 function adams(obj, fonctions) {
     let infoComs = obj;
     if (!obj.categorie) infoComs.categorie = "General";
-    if (!obj.reaction) infoComs.reaction = "🚘"; // Default reaction
+    if (!obj.reaction) infoComs.reaction = "🚀"; // Default reaction
     infoComs.fonction = fonctions;
     cm.push(infoComs);
     return infoComs;
